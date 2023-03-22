ba8def440ab4ea7ff257b7716ff57dfed5bb4da8
arthurcsp <arthurcspinto@hotmail.com>
2023-03-22 19:10:39 -0300

    adding test files

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculate functions
+"""
+
+
+def add(x , y):
+    """Add two values."""
+    return x + y
+
+def substract(x , y):
+    """Substract two values."""
+    return y - x
---
+++ app/app/test.py
@@ -0,0 +1,21 @@
+""" 
+Unit tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test the add function."""
+        result = calc.add(5 , 6)
+
+        self.assertEqual(result , 11)
+
+    def test_substract_number(self):
+        """Teste the substract function"""
+        result = calc.substract(10 , 15)
+
+        self.assertEqual(result , 5)
