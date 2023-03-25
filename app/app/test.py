e7760f27eb05d97a499c2987db897ecc3413b705
arthurcsp <arthurcspinto@hotmail.com>
2023-03-25 17:28:41 -0300

    lint corrections

--- app/app/test.py
+++ app/app/test.py
@@ -1,4 +1,6 @@
 """ 
+
+
 Unit tests
 """
 from django.test import SimpleTestCase
@@ -10,12 +12,12 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test the add function."""
-        result = calc.add(5 , 6)
+        result = calc.add(5,6)
 
-        self.assertEqual(result , 11)
+        self.assertEqual(result,11)
 
     def test_substract_number(self):
         """Teste the substract function"""
-        result = calc.substract(10 , 15)
+        result = calc.substract(10,15)
 
-        self.assertEqual(result , 5)
+        self.assertEqual(result,5)
